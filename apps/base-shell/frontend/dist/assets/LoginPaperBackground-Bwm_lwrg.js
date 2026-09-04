4f67d08e713481bae7c5a5a3294242dd193b9c24
giuntiocram <giuntiocram@users.noreply.github.com>
2026-09-04 13:03:54 +0000

    build(base-shell): publish repeatable auth revocation

--- apps/base-shell/frontend/dist/assets/LoginPaperBackground-Bwm_lwrg.js
+++ apps/base-shell/frontend/dist/assets/LoginPaperBackground-Bwm_lwrg.js
@@ -1,4 +1,4 @@
-import{r as f,j as R}from"./index-CIFrIwjE.js";const P=`#version 300 es
+import{r as f,j as R}from"./index-BTjuS6tZ.js";const P=`#version 300 es
 precision mediump float;
 
 layout(location = 0) in vec4 a_position;

0705d55c630377b2d42ee7533b9087b5373c2f27
giuntiocram <giuntiocram@users.noreply.github.com>
2026-09-04 11:38:43 +0000

    build(base-shell): publish atomic cache transitions

--- apps/base-shell/frontend/dist/assets/LoginPaperBackground-C_4oxjzp.js
+++ apps/base-shell/frontend/dist/assets/LoginPaperBackground-C_4oxjzp.js
@@ -1,4 +1,4 @@
-import{r as f,j as R}from"./index-DwRtFU-4.js";const P=`#version 300 es
+import{r as f,j as R}from"./index-CIFrIwjE.js";const P=`#version 300 es
 precision mediump float;
 
 layout(location = 0) in vec4 a_position;
